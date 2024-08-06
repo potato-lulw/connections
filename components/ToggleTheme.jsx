6ae65d8210be51c8bd52b38f5dd40682a30b8dac
potato-lulw <om.patil17072@gmail.com>
2024-08-07 02:34:29 +0530

    Initial Commit

---
+++ components/ToggleTheme.jsx
@@ -0,0 +1,18 @@
+"use client"
+import { useTheme } from 'next-themes'
+import React from 'react'
+import { Button } from './ui/button';
+import { Moon, Sun } from 'lucide-react';
+
+const ToggleTheme = () => {
+    const { theme, setTheme } = useTheme();
+    return (
+        <Button variant="outline" size={"icon"} onClick={() => setTheme(theme === "dark" ? "light" : "dark")} aria-label="Toggle Theme">
+            <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
+            <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only"></span>
+        </Button>
+    )
+}
+
+export default ToggleTheme
